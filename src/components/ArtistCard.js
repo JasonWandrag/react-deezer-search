1cd52cbc8cb10cf1070d5536fc73c747e591e2a2
JasonWandrag <jasonwandrag@hotmail.com>
2022-02-02 00:29:57 +0200

    Added extra fetch calls for Artist view to fetch Top Tracks and Albums

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -5,8 +5,12 @@ const ArtistCard = (props) => {
   const artist = props.artist;
   return (
     <Link to={"/artists/" + artist.id}>
-      {console.log(artist)}
-      <h3>{artist.name}</h3>
+      <div className="artist-card">
+        <img src={artist.picture_small} alt={artist.name} />
+        <h3>{artist.name}</h3>
+        <p>Fans: {artist.nb_fan}</p>
+        <p>Albums: {artist.nb_album}</p>
+      </div>
     </Link>
   );
 };
--- src/views/Artist.js
+++ src/views/Artist.js
@@ -6,20 +6,67 @@ const Artist = () => {
   const { id } = useParams();
 
   const [artist, setArtist] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
+  const [albums, setAlbums] = useState(null);
 
   useEffect(() => {
     fetch(
-      "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/" + id
+      `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}`
     )
       .then((res) => res.json())
-      .then((data) => {
-        setArtist(data);
-      });
+      .then((artist) => {
+        setArtist(artist);
+        return fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top`
+        );
+      })
+      .then((res) => res.json())
+      .then((topTracks) => {
+        console.table(topTracks.data);
+        setTopTracks(topTracks.data);
+        return fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/albums`
+        );
+      })
+      .then((res) => res.json())
+      .then((albums) => {
+        console.log(albums.data);
+        setAlbums(albums.data);
+      })
+      .catch((e) => console.log(e));
   }, []);
   return (
     <div>
       Single Artist Page - {id}
-      {artist && <div>{artist.name}</div>}
+      {artist && (
+        <div>
+          <img src={artist.picture_small} alt={artist.name} />
+          <h3>{artist.name}</h3>
+          <p>Fans: {artist.nb_fan}</p>
+          {topTracks && (
+            <div>
+              <h3>Top 5</h3>
+              {topTracks.map((track, idx) => (
+                <div key={track.id}>
+                  {idx}: {track.title} - {track.duration}
+                </div>
+              ))}
+            </div>
+          )}
+          {albums && (
+            <div>
+              <h3>All Albums</h3>
+              {albums.map((album) => (
+                <div key={album.id}>
+                  <img src={album.picture_small} alt={album.title} />
+                  <p>{album.title}</p>
+                  <p>{album.release_date}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
